6a02b945c9358f99a4d49245f05a67117eaa8c54
unknown <osamamatar7@gmail.com>
2021-03-26 17:05:22 +0200

    create new interface

--- POS/UI/SplashSc.Designer.cs
+++ POS/UI/SplashSc.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace POS
+﻿namespace POS.UI
 {
-    partial class Form1
+    partial class SplashSc
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "SplashSc";
         }
 
         #endregion
     }
 }
-
--- POS/UI/SplashSc.cs
+++ POS/UI/SplashSc.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace POS
+namespace POS.UI
 {
-    public partial class Form1 : Form
+    public partial class SplashSc : Form
     {
-        public Form1()
+        public SplashSc()
         {
             InitializeComponent();
         }
